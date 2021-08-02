46eb4856018cbdfd3b0c071c4ede8fce0b7540a6
Carey Dayrit <carey.dayrit@pantheon.io>
2021-08-03 05:25:49 +0800

    fetch without cache

--- homepage.py
+++ homepage.py
@@ -1,14 +1,14 @@
-import os
-import string
-import random
+# Author: Carey Dayrit
+# Description: Homepage without cache
 
-from locust import HttpLocust, TaskSet, task, between
+import time
 
-class MyTaskSet(TaskSet):
-	@task
-	def index(self):
-		response = self.client.get("/")
+from locust import HttpUser, task, between
+     
+class MyLocust(HttpUser):    	
+    wait_time = between(1, 10)    
+    
+    @task
+    def homepage(self):
+        self.client.get("/", headers={"User-Agent":"python-requests/2.6.0 CPython/2.7.5 Linux/3.10.0-862.14.4.el7.x86_64", "Cookie": "no-cache"})
         
-class MyLocust(HttpLocust):
-	task_set = MyTaskSet
-	wait_time = between(1, 10)
